e2bbbf41a2a11846eb1a1a9b39535e8e88cc93d2
trishane <trishanereid@gmail.com>
2024-07-26 12:01:58 +0530

    first commit

---
+++ src/main/java/edu/icet/rental_management_system/RentalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.icet.rental_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalManagementSystemApplication.class, args);
+	}
+
+}
